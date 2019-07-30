d351b0ee90fc7a021aa138ad8b55f4db0b83bf93
myname21-meet <myname21@meet.mit.edu>
2019-07-30 11:29:19 -0400

    flaskforms

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from databases import *
 from flask import Flask, render_template, url_for, request
 app = Flask(__name__)
 
+
+
+
 @app.route('/')
 def home():
     return render_template('home.html', students=query_all())
@@ -10,7 +13,17 @@ def home():
 def display_student(student_id):
     return render_template('student.html', student=query_by_id(student_id))
 
-#Create an '/add' route here:
+@app.route('/add', methods=['GET', 'POST'])
+def add_student_route():
+	if request.method == 'GET':
+		return render_template('home.html')
+	else:
+		name = request.form['student_name']
+		year = request.form['student_year']
+		add_student(name, year)
+		return ('Received POST request!')
+
+@app.route('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
